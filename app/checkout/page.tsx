5a3d6707f36311ba2ac95623c493e58279e71400
faithnextvision-png <FaithNextVision@gmail.com>
2026-07-09 03:17:44 +0530

    fix: resolve checkout page module type error

---
+++ app/checkout/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CheckoutPage() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <p>Your checkout content will go here.</p>
+    </div>
+  )
+}
